c27414f35b7bfb7c6e4307820b7723b71eba8ab5
Arthur Faria Peixoto <arthurfpeixoto@gmail.com>
2021-02-14 19:46:39 -0300

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,20 +4,30 @@ public class Player {
 
     public boolean right, left;
     public int x, y;
+    public int WIDTH, HEIGHT;
+    public double speed = 2.0;
 
     public Player(int x, int y){
         this.x = x;
         this.y = y;
+        this.WIDTH = 40;
+        this.HEIGHT = 5;
     }
     public void tick() {
         if (right) {
-            x++;
+            x+=speed;
         } else if (left) {
-            x--;
+            x-=speed;
+        }
+        if(x+WIDTH > Game.WIDTH){
+            x = Game.WIDTH - WIDTH;
+        }
+        else if(x < 0){
+            x = 0;
         }
     }
     public void render(Graphics g){
-        g.setColor(Color.WHITE);
-        g.fillRect(x, y, 40, 10);
+        g.setColor(Color.BLUE);
+        g.fillRect(x, y, WIDTH, HEIGHT);
     }
 }
